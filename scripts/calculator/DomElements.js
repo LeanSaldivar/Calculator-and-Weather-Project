acba1a3c64d96a7ecbf3bb706e5515e8eaf7a924
LeanSaldivar <leanemmanuelsaldivar@gmail.com>
2025-05-03 20:26:18 +0800

    Refactor calculator styles and add math functionality
    Abstract styles into reusable mixins for buttons and grid layouts, improving maintainability. Introduce `GetInput.js` for handling calculator input and math operations using the `mathjs` library. Update dependencies to include `mathjs` and related packages for enhanced mathematical capabilities.

---
+++ scripts/calculator/DomElements.js
@@ -0,0 +1,12 @@
+let numButton = document.querySelectorAll('.number-button');
+let operatorButton = document.querySelectorAll('.operation-button');
+let inputText = document.querySelector('#input-text');
+let divideButton = document.querySelector('#divide-button');
+let clearButton = document.querySelector('#clear-button');
+let percentButton = document.querySelector('#percent-button');
+let deleteButton = document.querySelector('#delete-button');
+let equalsButton = document.querySelector('#equals-button');
+
+export {numButton, operatorButton, inputText,
+    divideButton, clearButton, percentButton,
+    deleteButton, equalsButton};
---
+++ scripts/calculator/GetInput.js
@@ -0,0 +1,17 @@
+import * as math from 'https://esm.sh/mathjs';
+
+const getInput = (num, inputText) => inputText.value += num;
+
+const clear = (inputText) => inputText.value = "";
+
+const del = (inputText) => inputText.value = inputText.value.slice(0, -1);
+
+const calculateResult = (inputText) => {
+    try {
+        inputText.value = math.evaluate(inputText.value);
+    } catch (e) {
+        inputText.value = "Syntax error";
+    }
+};
+
+export { getInput, clear, del, calculateResult };
