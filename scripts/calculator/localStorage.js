cf7324d806c5526bbf7c80d4cc738363dfdfc11e
LeanSaldivar <leanemmanuelsaldivar@gmail.com>
2025-05-05 20:13:10 +0800

    Add calculator history modal with localStorage integration
    Implemented a history modal to display past calculations using localStorage. Enhanced the dialog's UI and styling, updated JavaScript logic for history retrieval, and refined layouts for improved user interaction.

--- scripts/calculator/localStorage.js
+++ scripts/calculator/localStorage.js
@@ -60,10 +60,6 @@ function getHistoryInput(historyInput, inputText) {
     }
 }
 
-function createElement() {
-
-}
-
 const getLocalHistory = () => {
     const historyContainer = document.querySelector(".history-log");
     const history = localStorage.getItem("calculations");
@@ -80,7 +76,7 @@ const getLocalHistory = () => {
         const deleteLogContainer = document.createElement("div");
         deleteLogContainer.classList.add("delete-log");
         deleteLogContainer.innerHTML =`
-            <svg className="trash" id="trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            <svg class="trash" id="trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                  viewBox="0 0 24 24">
                 <path fill="currentColor"
                       d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"/>
